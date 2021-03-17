eb86519c8c72560180e13fede9474dccc146c297
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-03-17 09:27:41 +0300

    Work with widgets 2

--- lb4_5/lib/Trip/TripElement.dart
+++ lb4_5/lib/Trip/TripElement.dart
@@ -2,6 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TripElement extends StatelessWidget {
+  final String name;
+  final String seasonAndDuration;
+  final Color myColor;
+
+  const TripElement( {Key key,
+    this.name,
+    this.myColor,
+    this.seasonAndDuration} ) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -9,9 +17,9 @@ class TripElement extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
       color: Colors.green,
       child: Text(
-        "Trip element test",
+        name,
         textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 24, color: Colors.indigoAccent),
+        style: TextStyle(fontSize: 24, color: myColor),
         textAlign: TextAlign.center,
       ),
     );
--- lb4_5/lib/main.dart
+++ lb4_5/lib/main.dart
@@ -1,26 +1,14 @@
 import 'package:flutter/material.dart';
-import 'MainPage.dart';
-import 'TripElement.dart';
+import 'Pages/MainPage.dart';
+import 'Trip/TripElement.dart';
 
 void main() {
-  runApp(TripElement());
+  runApp(
+      TripElement(
+      name: "Some text",
+      myColor: Colors.indigoAccent)
+  );
 }
-
-// Container Widget
-
-/*
-Container(
-padding: EdgeInsets.only(top:50, left:10, right:10),
-color: Colors.white,
-child: Text("Hello Flutter from Metanit.com",
-textDirection: TextDirection.ltr,
-textAlign: TextAlign.center,
-style: TextStyle(color: Colors.amber,
-fontSize: 24,
-backgroundColor: Colors.black87
-)
-)
-)*/
 
 /*
 class MyApp extends StatelessWidget {

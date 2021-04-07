8b91b1398dab332c321ecdda551e01fdaf57048c
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-04-07 10:34:10 +0300

    Try function which cteare elements for listview

--- lb4_5/lib/Trip/TripList.dart
+++ lb4_5/lib/Trip/TripList.dart
@@ -5,13 +5,47 @@ import 'TripElement.dart';
 
 class TripList extends StatelessWidget {
 
+  final List<String> names;
+  final List<String> seasonsAndDurations;
+  final List<Color> textColors;
+  final List<String> backgroundImagesNames;
+  final List<String> seasonsImages;
+  final List< List<String> > peoplesImages;
+
+  const TripList(
+      {
+        Key key,
+        this.names,
+        this.seasonsAndDurations,
+        this.textColors,
+        this.backgroundImagesNames,
+        this.seasonsImages,
+        this.peoplesImages,
+      } ) : super(key: key);
+
+  ListView fillList(int elementAmount){
+    return ListView.builder(
+        itemCount: elementAmount,
+        itemBuilder: (context, index){
+          return TripElement(
+            name: names.elementAt(index),
+            seasonAndDuration: seasonsAndDurations.elementAt(index),
+            textColor: textColors.elementAt(index),
+            backgroundImageName: backgroundImagesNames.elementAt(index),
+            seasonImage: seasonsAndDurations.elementAt(index),
+            peopleImages: peoplesImages.elementAt(index),
+          );
+        }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 560,
       margin: EdgeInsets.only(left: 15.0, right: 15.0),
-      child: ListView(
-        children: [
+      child: /*ListView(
+        children: <Widget>[
           TripElement(
             name: "Luksemburg",
             seasonAndDuration: "Summer 2021 - 6 days",
@@ -45,7 +79,7 @@ class TripList extends StatelessWidget {
             peopleImages: ["images/peoples/man1.jpg"],
           )
         ],
-      ),
+      ),*/
     );
   }
 }

30f349647eeb706dc2140ffa45117cecce6c6b1e
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-04-08 08:44:26 +0300

    Added small app for showcase of special functions
    Will show only for Android app

--- lb4_5/lib/main.dart
+++ lb4_5/lib/main.dart
@@ -1,7 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'Pages/InfoPage.dart';
 import 'Pages/MainPage.dart';
 
 void main() {
-  runApp(MainPage());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.white),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: PageViewerWidget(),
+      ),
+    );
+  }
+}
+
+class PageViewerWidget extends StatelessWidget {
+  const PageViewerWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final PageController controller = PageController(initialPage: 0);
+    return PageView(
+      scrollDirection: Axis.horizontal,
+      controller: controller,
+      children: <Widget>[
+        Center(child: MainPage()),
+        Center(child: InfoPage()),
+      ],
+    );
+  }
 }

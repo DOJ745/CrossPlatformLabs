0daee8ddd178a82f9e5abc8e50e02a50c5970285
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-04-21 17:21:22 +0300

    Loaded LB7_8

---
+++ lb7_8/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:lb7_8/pages/SQFLitePage.dart';
+import 'package:lb7_8/pages/SharedPreferPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.white),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: PageViewerWidget(),
+      ),
+    );
+  }
+}
+
+class PageViewerWidget extends StatelessWidget {
+
+  const PageViewerWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final PageController controller = PageController(initialPage: 0);
+    return PageView(
+      scrollDirection: Axis.horizontal,
+      controller: controller,
+      children: <Widget>[
+        Center(
+            child: SQFLitePage()
+        ),
+        Center(
+            child: SharedPreferPage()
+        ),
+      ],
+    );
+  }
+}

b7d1efeba18d6573066d8836f7775393a607b8c5
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-04-21 11:24:11 +0300

    Small work

--- lb7_8/lib/pages/SQFLitePage.dart
+++ lb7_8/lib/pages/SQFLitePage.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:lb7_8/model/DeepHouse.dart';
 
 class SQFLitePage extends StatelessWidget {
 
+  final DeepHouseTrack track;
+
+  SQFLitePage(
+      {
+        Key key,
+        this.track,
+      } ) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Column(
+      children: [
+        TextFormField(
+
+        ),
+      ],
+    );
   }
 
 }

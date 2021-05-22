2136f2104bbde1552da1f3c51151e490849ffae1
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-05-22 11:11:36 +0300

    Going to receive messaging from firebase

---
+++ lb10/android/app/src/main/kotlin/com/faa/lb10/Application.kt
@@ -0,0 +1,18 @@
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
+
+class Application : FlutterApplication(), PluginRegistrantCallback {
+
+    @Override
+    fun onCreate() {
+        super.onCreate()
+        FlutterFirebaseMessagingService.setPluginRegistrant(this)
+    }
+
+    @Override
+    fun registerWith(registry: PluginRegistry?) {
+        FirebaseCloudMessagingPluginRegistrant.registerWith(registry)
+    }
+}
